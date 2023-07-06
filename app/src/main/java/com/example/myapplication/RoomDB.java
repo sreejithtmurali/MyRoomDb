ba1baae86329ab83a122954e76d3626014aaa394
Sreeith Murali <sreejithtmurali@gmail.com>
2023-07-06 16:29:25 +0530

    secoond

---
+++ app/src/main/java/com/example/myapplication/RoomDB.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {MainData.class},version = 1,exportSchema = false)
+public abstract class RoomDB extends RoomDatabase {
+    //Create Database instance
+    private static RoomDB database;
+    private static final String DATABASE_NAME="database";
+    public synchronized static RoomDB getInstance(Context context){
+        if(database==null){
+            //when database is null initialise database
+            database= Room.databaseBuilder(context.getApplicationContext(),RoomDB.class,DATABASE_NAME)
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build();
+
+        }
+        //return Database
+        return database;
+    }
+    public abstract MainDao mainDao();
+}
